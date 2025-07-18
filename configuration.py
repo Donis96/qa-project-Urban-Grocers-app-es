a115b94c2c61413b6e2070d70b2b24a6a42366cd
Donis96 <donismarincesar@gmail.com>
2025-07-18 17:20:04 -0600

    sube los archivos

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-5fd5ab91-4f53-48e8-b04d-12d789f96355.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-e02f9487-6636-4d15-8d91-a10f02bf9bec.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
